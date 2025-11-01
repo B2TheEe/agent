25757c9f03d1a392ab6b70ab5bd5595e3578ed8d
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-11-01 15:24:14 +0100

    Adding function tools to agent

--- cve_agent.py
+++ cve_agent.py
@@ -1,13 +1,14 @@
 from itertools import product
 from trace import Trace
 import requests
-from agents import Agent, Runner, trace
+from agents import Agent, Runner, trace, function_tool
 from openai.types.responses import ResponseTextDeltaEvent
 import asyncio
-
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
+@function_tool
 def get_cpe(product: str) -> str:
      """
       CPE finder
@@ -15,12 +16,20 @@ def get_cpe(product: str) -> str:
      :return: CPE
      """
      a = "https://services.nvd.nist.gov/rest/json/cpes/2.0?keywordSearch={0}&keywordExactMatch".format(product)
-     print(a)
      x = requests.get(a)
-     print(x.status_code)
-     print(x.request)
+     z = x.json()
+     print(z)
+     products = z["products"]
+     if len(products) == 0:
+         return "No CPE result"
+     else:
+         cpes = products[0]
+         cpename = cpes["cpe"]["cpeName"]
+         print(cpename)
+         return cpename
 
 
+@function_tool
 def get_cve(product: str) -> str:
     """
     Get CVE's from a prpduct
@@ -28,22 +37,29 @@ def get_cve(product: str) -> str:
     :param product: Name of the product
     :return: CVE's
     """
+    r = "https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={0}".format(product)
+    x = requests.get(r)
+    z = x.json()
+    print(z)
 
 
 async def main():
- cve_agent = Agent(name="CVE Agent", instructions="You will provide information about vulnerabilities from a specific product,  Your result is information about CVE's from the NVD. ")
+ cve_agent = Agent(name="CVE Agent",
+                   instructions="You will provide information about vulnerabilities from a specific product,  Your result is information about CVE's from the NVD. ",
+                   tools= {get_cpe, get_cve})
  with trace("Simple CVE agent"):
-     result = await Runner.run(cve_agent, "Find all the vulnerabilities from Apache")
+     result = await Runner.run(cve_agent, "Find all the vulnerabilities from Ubuntu Linux 24.04 LTS Edition")
 
      print(result)
 
 if __name__ == "__main__":
-    get_cpe("Ubuntu")
-
     """ 
+   cpe = get_cpe("Ubuntu Linux 24.04 LTS Edition")
+   get_cve(cpe)
+"""
 
     asyncio.run(main())
 
-    
+    """ 
     This product uses the NVD API but is not endorsed or certified by the NVD."
     """
